3c4d14df4cf03c9045ff5d8a38c8344a703153c9
Karan Barsiwal <barsiwal.karan@tftus.com>
2017-08-01 17:06:20 +0530

    sada

---
+++ app/src/main/java/com/example/barsiwalkaran/abhi/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.barsiwalkaran.abhi;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+public class MainActivity extends AppCompatActivity {
+    LoginButton loginButton;
+    private CallbackManager callbackManeger;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        setContentView(R.layout.activity_main);
+        loginButton = (LoginButton) findViewById(R.id.login_button);
+        LoginManager.getInstance().registerCallback(callbackManeger, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+
+            }
+
+            @Override
+            public void onCancel() {
+
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+
+            }
+        });
+    }
+
+}
